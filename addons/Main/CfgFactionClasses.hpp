a676df078ed9b5f08330daaaf3f5dbfa73d44940
YonV <32718226+YonVclaw@users.noreply.github.com>
2021-06-21 19:41:29 -0600

    10_3

--- addons/Main/CfgFactionClasses.hpp
+++ addons/Main/CfgFactionClasses.hpp
@@ -1,14 +1,32 @@
-class CfgFactionClasses
-{
-	class NO_CATEGORY;
-	class DPSO_Modules: NO_CATEGORY
+class CfgFactionClasses {    
+    class NO_CATEGORY;
+
+    class DPSO: NO_CATEGORY
+    {
+        displayName = PREFIX;
+    };
+    class PREFIX: NO_CATEGORY {
+        displayName = PREFIX;
+    };
+    class DPSO_Modules: NO_CATEGORY
 	{
-		displayName = "DPSO";
+		displayName = PREFIX;
 	};
-	class PREFIX: NO_CATEGORY {
-		displayName = "DPSO";
-		icon = "\z\dpso\addons\media\images\logo_sm_ca.paa";
-		priority = 8;
-		side = 7;
+    class dpso: NO_CATEGORY {
+        displayName = PREFIX;
+        priority = 2;
+        side = 7;
+    };
+    class DPSO_Faction: NO_CATEGORY {
+		displayName = PREFIX; //Faction name in-game
+		icon = LOGO_PATH;
+		priority = 0;
+		side = 1; // 1 Blufor 2 opfor 3 independant i think.
 	};
+	class CLASS(IND) {
+        displayName = PREFIX;
+        icon = LOGO_PATH;
+        side = 2;
+        priority = 2;
+    };
 };
--- addons/Main/script_version.hpp
+++ addons/Main/script_version.hpp
@@ -1,5 +1,5 @@
 
 #define MAJOR 10
 #define MINOR 3
-#define PATCH 2
+#define PATCH 3
 #define BUILD 1
